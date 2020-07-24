4f1cc02d6f56f2b36da30c2a1c36342e9c989308
Shadman Shadab <oneshadab@gmail.com>
2020-07-25 00:27:14 +0600

    Add colors

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -20,12 +20,50 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .home {
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
 
+  background-color: #333333;
+  height: 100vh;
+  width: 100vw;
+
+  .logo {
+    h1 {
+      font-size: 189px;
+      color: white;
+    }
+  }
+
+  .url-input {
+    input {
+      font-size: 30px;
+      border-radius: 50px;
+      padding: 15px 25px;
+
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+
+  .shrink-button {
+    button {
+      background-color: #2D9CDB;
+      color: white;
+      font-size: 30px;
+      border-radius: 50px;
+      min-width: 150px;
+      border: none;
+    }
+  }
 }
+
+
+
+
+
 </style>
