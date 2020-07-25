ceac6cd657e2d6b53da9b522dd6a42fba72424fd
Shadman Shadab <oneshadab@gmail.com>
2020-07-25 15:56:05 +0600

    Save url to database

--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -2,9 +2,16 @@ require 'digest'
 
 class UrlController < ApplicationController
   def shrink
-    url_digest = Digest::SHA2.hexdigest params[:url]
-    url_guid = url_digest[0..3]
+    original_url = params[:url]
+    url_digest = Digest::SHA2.hexdigest original_url
+    shrunk_url = url_digest[0..3]
 
+    # Url.create(original: original_url, shrunk: shrunk_url)
+
+    render plain: _shrunk_url_for(shrunk_url)
+  end
+
+  def _shrunk_url_for(url_guid)
     new_url = url_for(
       :controller => 'url',
       :action => 'goto',
@@ -12,7 +19,7 @@ class UrlController < ApplicationController
     )
 
     new_url = _remove_prococol(new_url)
-    render plain: new_url
+    return new_url
   end
 
   def _remove_prococol(url)
