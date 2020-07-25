ae58b4e1708c09cd93cfe43156501a6d3737739f
Shadman Shadab <oneshadab@gmail.com>
2020-07-25 16:31:54 +0600

    Move to helper methods

--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -1,48 +1,25 @@
 require 'digest'
 
 class UrlController < ApplicationController
+  include UrlHelper
+
   def shrink
     original_url = params[:url]
     url_digest = Digest::SHA2.hexdigest original_url
-    shrunk_guid = url_digest[0..3]
+    short_url_digest = url_digest[0..3]
 
-    Url.create(original: original_url, shrunk: shrunk_guid)
-
-    render plain: _shrunk_url_for(shrunk_guid)
+    url = Url.create(original: original_url, shrunk: short_url_digest)
+    render plain: digest_to_url(url.shrunk)
   end
 
   def goto
     url = Url.find_by(shrunk: params[:guid])
-    redirect_to(url.original)
+    redirect_to(cleaned(url.original))
   end
 
-
-  def _shrunk_url_for(url_guid)
-    new_url = url_for(
-      :controller => 'url',
-      :action => 'goto',
-      :guid => url_guid,
-    )
-
-    new_url = _remove_protocol(new_url)
-    return new_url
+  def digest_to_url(digest)
+    shrunk_url = url_for(controller: 'url', action: 'goto', guid: digest)
+    shrunk_url = remove_protocol(shrunk_url)
+    return shrunk_url
   end
-
-  def _set_protocol(url, protocol)
-    return protocol + "://" + _remove_protocol(url) # Remove any existing protocols
-  end
-
-
-  def _remove_protocol(url)
-    if !_has_protocol(url)
-      return url
-    end
-
-    return url.split('://')[1..].join('://')
-  end
-
-  def _has_protocol(url)
-    return url.split('://').length > 1
-  end
-
 end
--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -1,2 +1,26 @@
 module UrlHelper
+  def cleaned(url)
+    if has_protocol(url)
+      return url
+    end
+
+    return set_protocol(url, 'http')
+  end
+
+  def set_protocol(url, protocol)
+    url = remove_protocol(url) # Remove any existing protocols
+    return protocol + "://" + url
+  end
+
+  def remove_protocol(url)
+    if !has_protocol(url)
+      return url
+    end
+
+    return url.split('://')[1..].join('://')
+  end
+
+  def has_protocol(url)
+    return url.split('://').length > 1
+  end
 end
