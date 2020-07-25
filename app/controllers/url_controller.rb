f371ea4cf06f403dc7afaec48e584687e80e0e17
Shadman Shadab <oneshadab@gmail.com>
2020-07-25 14:18:15 +0600

    Rename methods

--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -2,13 +2,13 @@ require 'digest'
 
 class UrlController < ApplicationController
   def shrink
-    url_hash = Digest::SHA2.hexdigest params[:url]
-    url_hash_prefix = url_hash.to_s[0..3]
+    url_digest = Digest::SHA2.hexdigest params[:url]
+    url_guid = url_digest[0..3]
 
     new_url = url_for(
       :controller => 'url',
-      :action => 'redirect',
-      :url => url_hash_prefix
+      :action => 'goto',
+      :guid => url_guid
     )
 
     render plain: new_url
