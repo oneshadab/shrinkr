baa2f198665988046db13f87c98f0e8f263432fb
Shadman Shadab <oneshadab@gmail.com>
2020-07-25 14:11:59 +0600

    Return hashed url

--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -1,6 +1,16 @@
+require 'digest'
+
 class UrlController < ApplicationController
   def shrink
-    @url = params[:url] + 'done!'
-    render plain: @url
+    url_hash = Digest::SHA2.hexdigest params[:url]
+    url_hash_prefix = url_hash.to_s[0..3]
+
+    new_url = url_for(
+      :controller => 'url',
+      :action => 'redirect',
+      :url => url_hash_prefix
+    )
+
+    render plain: new_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   post '/api/shrink/:url', to: 'url#shrink'
 
+  get '/:url', to: 'url#redirect', as: 'redirect'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
