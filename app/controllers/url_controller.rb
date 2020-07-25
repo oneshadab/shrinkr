adf3bb2594401083554e7674855a9db37b11f3a2
Shadman Shadab <oneshadab@gmail.com>
2020-07-25 16:35:28 +0600

    use consistent naming

--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -8,17 +8,17 @@ class UrlController < ApplicationController
     url_digest = Digest::SHA2.hexdigest original_url
     short_url_digest = url_digest[0..3]
 
-    url = Url.create(original: original_url, shrunk: short_url_digest)
-    render plain: digest_to_url(url.shrunk)
+    url = Url.create(original: original_url, digest: short_url_digest)
+    render plain: digest_to_url(url.digest)
   end
 
   def goto
-    url = Url.find_by(shrunk: params[:guid])
+    url = Url.find_by(digest: params[:digest])
     redirect_to(cleaned(url.original))
   end
 
   def digest_to_url(digest)
-    shrunk_url = url_for(controller: 'url', action: 'goto', guid: digest)
+    shrunk_url = url_for(controller: 'url', action: 'goto', digest: digest)
     shrunk_url = remove_protocol(shrunk_url)
     return shrunk_url
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   post '/api/shrink', to: 'url#shrink'
 
-  get '/:guid', to: 'url#goto', as: 'goto'
+  get '/:digest', to: 'url#goto', as: 'goto'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
