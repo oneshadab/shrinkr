ae9e2ce0fd947719a0e000209c1b7cc267378b95
Shadman Shadab <oneshadab@gmail.com>
2020-07-31 18:09:34 +0600

    Add Status icon

---
+++ app/javascript/components/StatusIcon.vue
@@ -0,0 +1,48 @@
+<template>
+  <tooltip class="status-icon" message="That doesn't look like a valid url">
+    <icon :name="name" :color="color" :size="24" />
+  </tooltip>
+</template>
+
+<script>
+import Icon from './Icon';
+import Tooltip from './Tooltip';
+
+
+export default {
+  components: {
+    'icon': Icon,
+    'tooltip': Tooltip,
+  },
+
+  props: {
+    status: {
+      type: String,
+      default: "success"
+    }
+  },
+
+  computed: {
+    color() {
+      const colorFor = {
+        success: 'green',
+        warning: 'gray',
+        error: 'red',
+      };
+      return colorFor[this.status];
+    },
+    name() {
+      const nameFor = {
+        success: 'check',
+        warning: 'alert',
+        error: 'alert',
+      };
+      return nameFor[this.status];
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
