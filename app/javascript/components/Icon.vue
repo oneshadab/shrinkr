bdcb238af71ccb38457581d12ac64cd2bbc6429f
Shadman Shadab <oneshadab@gmail.com>
2020-07-31 14:37:12 +0600

    Add style props for Icon

--- app/javascript/components/Icon.vue
+++ app/javascript/components/Icon.vue
@@ -1,9 +1,49 @@
 <template>
-  <span v-html="require(`!html-loader!./../../assets/images/icons/${name}.svg`)"></span>
+  <span v-html="require(`!html-loader!./../../assets/images/icons/${name}.svg`)" :style="inlineStyle"></span>
 </template>
 
 <script>
 export default {
-  props: ['name']
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    size: {
+      type: Number,
+      default: 32,
+    },
+    color: {
+      type: String,
+      default: '#ffffff'
+    }
+  },
+
+  computed: {
+    inlineStyle() {
+      return {
+        '--color': this.color,
+        '--size': `${this.size}px`
+      }
+    }
+  }
 }
 </script>
+
+<style scoped>
+span {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+
+  height: var(--size);
+  width: var(--size);
+}
+
+span >>> svg {
+  height: var(--size);
+  width: var(--size);
+
+  fill: var(--color);
+}
+</style>
