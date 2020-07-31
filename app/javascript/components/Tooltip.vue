4be95088c775fe274bc9d245b23ada9b58f5ffc5
Shadman Shadab <oneshadab@gmail.com>
2020-07-31 17:59:54 +0600

    Add tooltip component

---
+++ app/javascript/components/Tooltip.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="tooltip">
+    <slot></slot>
+    <span class="tooltip-message">{{message}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.tooltip {
+  position: relative;
+  display: inline-block;
+
+  .tooltip-message {
+    background-color: #555;
+    border-radius: 6px;
+    bottom: 125%;
+    color: #fff;
+    left: 50%;
+    margin-left: -60px;
+    opacity: 0;
+    padding: 5px 0;
+    position: absolute;
+    text-align: center;
+    transition: opacity 0.3s;
+    visibility: hidden;
+    width: 120px;
+    z-index: 1;
+
+    &::after {
+      content: "";
+      position: absolute;
+      top: 100%;
+      left: 50%;
+      margin-left: -5px;
+      border-width: 5px;
+      border-style: solid;
+      border-color: #555 transparent transparent transparent;
+    }
+  }
+
+
+
+  &:hover {
+    .tooltip-message {
+      visibility: visible;
+      opacity: 1;
+    }
+  }
+}
+</style>
