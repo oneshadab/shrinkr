9d93dbed12c5b185170240cc9fa0333e391bae55
Shadman Shadab <oneshadab@gmail.com>
2020-08-01 12:35:39 +0600

    Update check for url validation

--- app/javascript/utils.js
+++ app/javascript/utils.js
@@ -1,10 +1,9 @@
 import validUrl from 'valid-url';
 
 function isValidUrl(url) {
-  return [
-    url,
-    `http://${url}`,
-  ].some(url => validUrl.isWebUri(url));
+  const regexString = "^(https?://)?(www\\.)?([-a-z0-9]{1,63}\\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.[a-z]{2,6}(/[-\\w@\\+\\.~#\\?&/=%]*)?$";
+  const urlRegex = new RegExp(regexString,"i");
+  return urlRegex.test(url);
 }
 
 export default {
