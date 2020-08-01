e439fe3a191d274f5f3d728dc760e1220ce7dd50
Shadman Shadab <oneshadab@gmail.com>
2020-08-01 11:57:19 +0600

    Change url format

--- app/javascript/service.js
+++ app/javascript/service.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const apiBase = '/api/';
+const apiBase = '/api';
 
 async function shrinkUrl(url) {
-  const res = await axios.post(`${apiBase}/shrink/`, { url });
+  const res = await axios.post(`${apiBase}/shrink`, { url });
   return res.data;
 }
 
